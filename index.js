065c46a4f818031cb88d81bcbbb7afc10956fe74
Jauhari-01 <shubham.jauhari009@gmail.com>
2023-08-19 20:40:08 +0530

    Done some changes

--- index.js
+++ index.js
@@ -1,30 +1,5 @@
 //student object
-const students = [
-    // {
-    //   ID: 1,
-    //   name: 'Alice',
-    //   age: 21,
-    //   grade: 'A',
-    //   degree: 'Btech',
-    //   email: 'alice@example.com'
-    // },
-    // {
-    //   ID: 2,
-    //   name: 'Bob',
-    //   age: 22,
-    //   grade: 'B',
-    //   degree: 'MBA',
-    //   email: 'bob@example.com'
-    // },
-    // {
-    //   ID: 3,
-    //   name: 'Charlie',
-    //   age: 20,
-    //   grade: 'C',
-    //   degree:'Arts',
-    //   email: 'charlie@example.com'
-    // }
-  ]; 
+const students = [ ]; 
 
 //getting refrences for use
 let form = document.getElementById("f");
